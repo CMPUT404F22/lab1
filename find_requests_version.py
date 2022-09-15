3fc7765fe86ade1957becacf174d7094d9b805b0
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2022-09-14 21:11:36 -0600

    Fetch the script from github and print itself

--- find_requests_version.py
+++ find_requests_version.py
@@ -5,3 +5,8 @@ print("requests version:", requests.__version__)
 # get the google homepage with requests
 response = requests.get("http://www.google.com/")
 print(response)
+
+# download this script from github and print itself
+url_on_github = "https://raw.githubusercontent.com/CMPUT404F22/lab1/master/find_requests_version.py"
+response = requests.get(url_on_github)
+print(response.text)
